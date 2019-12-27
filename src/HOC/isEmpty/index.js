5294ce795a30004ec1be089c71f319543eea8f03
moskalenkogm@gmail.com <moskalenkogm@gmail.com>
2019-12-27 08:43:57 +0300

    декоратор для проверки  на пустые данные

---
+++ src/HOC/isEmpty/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Empty } from 'antd';
+import Error from '../../components/Common/Error'
+
+const isEmpty = (data, renderFunc) => {
+  try {
+    if (data.length === 0) return <Empty />
+    else return data.map(renderFunc)
+  } catch(error) {
+    return <Error />
+  }
+
+}
+
+export default isEmpty;
